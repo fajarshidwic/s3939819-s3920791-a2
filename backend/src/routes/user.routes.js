61ded070a6c6f31e05b353b424979b46f2946686
fajarshidwic <s3920791@student.rmit.edu.au>
2024-06-03 12:47:34 +1000

    backend bug fix

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 module.exports = (express, app) => {
-    const controller = require("../controllers/user.controller.js");
+    const controller = require("../controller/user.controller.js");
     const router = express.Router();
   
     // Select all users.
